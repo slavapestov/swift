d8f71fd312c58d9bbcf993ff724a7f74611f3149
Slava Pestov <spestov@apple.com>
2016-12-23 09:18:55 -0500

    SILGen: Emit local types with a separate pass instead of while walking statements
    Otherwise we might miss emitting a local type that's inside
    unreachable code. Normally such a type cannot be found via
    name lookup either, but IRGen will walk the list of local types
    and try to emit a class with no SIL vtable, which will crash.
    
    Fixes <https://bugs.swift.org/browse/SR-1924>.

--- lib/SILGen/SILGenFunction.h
+++ lib/SILGen/SILGenFunction.h
@@ -1524,7 +1524,6 @@ public:
     llvm_unreachable("Not yet implemented");
   }
 
-  void visitNominalTypeDecl(NominalTypeDecl *D);
   void visitFuncDecl(FuncDecl *D);
   void visitPatternBindingDecl(PatternBindingDecl *D);
 
@@ -1533,6 +1532,10 @@ public:
   std::unique_ptr<Initialization>
   emitPatternBindingInitialization(Pattern *P, JumpDest failureDest);
     
+  void visitNominalTypeDecl(NominalTypeDecl *D) {
+    // No lowering support needed.
+  }
+
   void visitTypeAliasDecl(TypeAliasDecl *D) {
     // No lowering support needed.
   }
