25f1cd5d0b2f2436fbcc5dc5af7cfdaf3a015d81
Brent Royal-Gordon <brent@architechies.com>
2017-04-01 01:20:40 -0700

    Mark failed initializers as errors
    When the initialization expression on a variable declaration fails, give it an ErrorType so Swift does not redundantly diagnose its un-inferrable type.
    
    Fixes:
    
    * test/NameBinding/stdlib.swift
    * test/decl/typealias/protocol.swift
    
    Would also have redundantly fixed the same tests as 2e875e7e, but the targeted solution there is cleaner.

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -1962,6 +1962,10 @@ bool TypeChecker::typeCheckBinding(Pattern *&pattern, Expr *&initializer,
                                      contextualPurpose,
                                      flags,
                                      &listener);
+  
+  if (hadError && !initializer->getType()) {
+    initializer->setType(ErrorType::get(Context));
+  }
 
   if (hadError && !pattern->hasType()) {
     pattern->setType(ErrorType::get(Context));
