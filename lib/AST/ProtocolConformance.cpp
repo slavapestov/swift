69577f8f6a47386cc84ace430f79e9f71df289e1
Slava Pestov <spestov@apple.com>
2017-04-13 02:40:40 -0700

    AST: Introduce ASTContext::getAnyObjectType()

--- lib/AST/ProtocolConformance.cpp
+++ lib/AST/ProtocolConformance.cpp
@@ -124,7 +124,7 @@ ProtocolConformanceRef::subst(Type origType,
   }
 
   // If that didn't find anything, we can still synthesize AnyObject
-  // conformances from thin air.  FIXME: gross.
+  // conformances from thin air.  FIXME: this is going away soon.
   if (proto->isSpecificProtocol(KnownProtocolKind::AnyObject)) {
     if (substType->isExistentialType())
       return *this;
@@ -777,6 +777,7 @@ void NominalTypeDecl::prepareConformanceTable() const {
 
   // Add any synthesized conformances.
   if (isa<ClassDecl>(this)) {
+    // FIXME: This is going away soon.
     if (auto anyObject = getASTContext().getProtocol(
                            KnownProtocolKind::AnyObject)) {
       ConformanceTable->addSynthesizedConformance(mutableThis, anyObject);
