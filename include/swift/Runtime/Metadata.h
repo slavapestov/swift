1d632185a669b7d93b5d270aa81431b6f0fda816
Slava Pestov <spestov@apple.com>
2017-08-21 18:06:09 -0700

    IRGen/Runtime: Use relative pointers in default witness table entries
    This to shrink the size of the protocol descriptor, removes a 32-bit
    padding field, and reduces the number of relocations.

--- include/swift/Runtime/Metadata.h
+++ include/swift/Runtime/Metadata.h
@@ -1476,7 +1476,7 @@ struct TargetNominalTypeDescriptor {
   ///
   /// Not all type metadata have access functions.
   TargetRelativeDirectPointer<Runtime, NonGenericMetadataAccessFunction,
-                              /*nullable*/ true> AccessFunction;
+                              /*Nullable*/ true> AccessFunction;
 
   /// A pointer to the generic metadata pattern that is used to instantiate
   /// instances of this type. Zero if the type is not generic.
@@ -2208,7 +2208,12 @@ struct TargetLiteralProtocolDescriptorList
   {}
 };
 using LiteralProtocolDescriptorList = TargetProtocolDescriptorList<InProcess>;
-  
+
+template <typename Runtime>
+struct TargetDefaultWitnessTableEntry {
+  TargetRelativeDirectPointer<Runtime, void *> Ptr;
+};
+
 /// A protocol descriptor. This is not type metadata, but is referenced by
 /// existential type metadata records to describe a protocol constraint.
 /// Its layout is compatible with the Objective-C runtime's 'protocol_t' record
@@ -2249,20 +2254,23 @@ struct TargetProtocolDescriptor {
   /// Only meaningful if ProtocolDescriptorFlags::IsResilient is set.
   uint16_t MinimumWitnessTableSizeInWords;
 
-  /// The maximum amount to copy from the default requirements in words.
-  /// If any requirements beyond MinimumWitnessTableSizeInWords are present
+  /// The maximum amount to copy from the default requirements in units of
+  /// 4 bytes.
+  ///
+  /// If any requirements beyond NumDefaultWitnessTableEntries are present
   /// in the witness table template, they will be not be overwritten with
   /// defaults.
   ///
   /// Only meaningful if ProtocolDescriptorFlags::IsResilient is set.
-  uint16_t DefaultWitnessTableSizeInWords;
+  uint16_t NumDefaultWitnessTableEntries;
 
-  /// Reserved. Really just here to zero-pad the structure on 64-bit.
-  uint32_t Reserved;
+  using DefaultWitnessTableEntry = TargetDefaultWitnessTableEntry<Runtime>;
+
+  DefaultWitnessTableEntry DefaultWitnessTable[];
 
   /// Default requirements are tail-allocated here.
-  void **getDefaultWitnesses() const {
-    return (void **) (this + 1);
+  void *getDefaultWitness(unsigned index) const {
+    return DefaultWitnessTable[index].Ptr.get();
   }
 
   constexpr TargetProtocolDescriptor<Runtime>(const char *Name,
@@ -2276,7 +2284,7 @@ struct TargetProtocolDescriptor {
       DescriptorSize(sizeof(TargetProtocolDescriptor<Runtime>)),
       Flags(Flags),
       MinimumWitnessTableSizeInWords(0),
-      DefaultWitnessTableSizeInWords(0)
+      NumDefaultWitnessTableEntries(0)
   {}
 };
 using ProtocolDescriptor = TargetProtocolDescriptor<InProcess>;
