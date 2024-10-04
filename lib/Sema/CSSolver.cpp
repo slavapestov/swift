ba7086bc3639db4f63b82afe58ba394ed167a900
Slava Pestov <spestov@apple.com>
2024-10-04 13:24:44 -0400

    Sema: Record case label items in the trail

--- lib/Sema/CSSolver.cpp
+++ lib/Sema/CSSolver.cpp
@@ -695,7 +695,6 @@ ConstraintSystem::SolverScope::SolverScope(ConstraintSystem &cs)
   numTypeVariables = cs.TypeVariables.size();
   numFixes = cs.Fixes.size();
   numKeyPaths = cs.KeyPaths.size();
-  numCaseLabelItems = cs.caseLabelItems.size();
   numPotentialThrowSites = cs.potentialThrowSites.size();
   numExprPatterns = cs.exprPatterns.size();
   numIsolatedParams = cs.isolatedParams.size();
@@ -738,9 +737,6 @@ ConstraintSystem::SolverScope::~SolverScope() {
 
   /// Remove any key path expressions.
   truncate(cs.KeyPaths, numKeyPaths);
-
-  // Remove any case label item infos.
-  truncate(cs.caseLabelItems, numCaseLabelItems);
 
   // Remove any potential throw sites.
   truncate(cs.potentialThrowSites, numPotentialThrowSites);
