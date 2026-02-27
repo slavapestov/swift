329acc5f06a22ba431d3ccf2d620b621827088ae
Slava Pestov <spestov@apple.com>
2026-02-27 16:22:09 -0500

    Sema: Add a case for unit tests
    The TestNoDoubleVoidClosureResultInference were actually not
    exercising the code path they were supposed to because we
    didn't check for that locator kind in getImpliedResultConversionKind().

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -7084,6 +7084,9 @@ static bool isDependentMemberTypeWithBaseThatContainsUnresolvedPackExpansions(
 
 ConstraintSystem::ImpliedResultConversionKind
 ConstraintSystem::getImpliedResultConversionKind(ConstraintLocator *locator) {
+  if (locator->isLastElement<LocatorPathElt::ClosureResult>())
+    return ImpliedResultConversionKind::ToVoid;
+
   if (locator->isLastElement<LocatorPathElt::ClosureBody>() ||
       locator->isForContextualType(CTP_ReturnStmt) ||
       locator->isForContextualType(CTP_ClosureResult) ||
