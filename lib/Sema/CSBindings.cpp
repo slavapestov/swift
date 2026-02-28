c3ce11e21e6c4b23a8568c8d4b94217648f2ffd1
Slava Pestov <spestov@apple.com>
2026-02-28 17:58:07 -0500

    Sema: Favor exact binding sets over conjunctions, too

--- lib/Sema/CSBindings.cpp
+++ lib/Sema/CSBindings.cpp
@@ -1895,6 +1895,11 @@ bool BindingSet::favoredOverConjunction(Constraint *conjunction) const {
       return false;
   }
 
+  if (!CS.shouldAttemptFixes()) {
+    if (getNumExactBindings() == 1)
+      return true;
+  }
+
   auto *locator = conjunction->getLocator();
   if (locator->directlyAt<ClosureExpr>()) {
     auto *closure = castToExpr<ClosureExpr>(locator->getAnchor());
