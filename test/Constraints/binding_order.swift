0be1bcfbbda1d12a1889c0a2a3064b1e1441e63d
Slava Pestov <spestov@apple.com>
2026-03-01 11:48:10 -0500

    Sema: Replace Type::join() call with subtypeJoin() in CSBindings.cpp

--- test/Constraints/binding_order.swift
+++ test/Constraints/binding_order.swift
@@ -23,7 +23,11 @@ do {
   // expected-error@-1 {{cannot convert value of type '[Any]' to expected argument type '[(any Command)?]'}}
   // expected-note@-2 {{arguments to generic parameter 'Element' ('Any' and '(any Command)?') are expected to be equal}}
   let _ = Array<any Command.Type>([Cut.self, Copy.self, Paste.self])
+  // expected-error@-1 {{cannot convert value of type '[Any]' to expected argument type '[any Command.Type]'}}
+  // expected-note@-2 {{arguments to generic parameter 'Element' ('Any' and 'any Command.Type') are expected to be equal}}
   let _ = Array<(any Command.Type)?>([Cut.self, Copy.self, Paste.self])
+  // expected-error@-1 {{cannot convert value of type '[Any]' to expected argument type '[(any Command.Type)?]'}}
+  // expected-note@-2 {{arguments to generic parameter 'Element' ('Any' and '(any Command.Type)?') are expected to be equal}}
 }
 
 // This expression first appeared in test/embedded/dict-init.swift.
