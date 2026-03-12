50af3fcaa8a31924f843155d50604026870f2fc6
Slava Pestov <spestov@apple.com>
2026-03-12 16:29:35 -0400

    update cgfloat test

--- validation-test/Sema/implicit_cgfloat_double_conversion_correctness.swift
+++ validation-test/Sema/implicit_cgfloat_double_conversion_correctness.swift
@@ -60,3 +60,18 @@ func testMultipleClosureInference(_ d: Double, i: Int) {
   func foo<T>(_: S<T>, _: T) {}
   foo(S({ CGFloat(i) }, { _ in }), d)
 }
+
+struct JoinIs<T> {}
+
+func testOptionalJoin(x1: Double, x2: CGFloat, y1: Double?, y2: CGFloat?) {
+  func same<T>(_: T, _: T) -> JoinIs<T> {}
+
+  let _: JoinIs<Double?> = same(x1, y1)
+
+  // This one was incorrectly CGFloat? before, violating the proposal's
+  // "widening conversion preferred" rule.
+  let _: JoinIs<Double?> = same(x1, y2)
+
+  let _: JoinIs<Double?> = same(x2, y1)
+  let _: JoinIs<CGFloat?> = same(x2, y2)
+}
